42472988d96894d1df71162d1e33e22340b670d7
David Alonso <125155276+DavidAlonsoRod@users.noreply.github.com>
2024-07-26 18:38:58 +0000

    Carta Aleatoria por David Alonso

--- src/app.js
+++ src/app.js
@@ -5,7 +5,23 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const numbers = ["A", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+const suits = ["♦", "♥", "♠", "♣"];
+
+function cardGenerator() {
+  const randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+
+  document.getElementById("number").innerHTML = randomNumber;
+  document.getElementById("suit-top").innerHTML = randomSuit;
+  document.getElementById("suit-bottom").innerHTML = randomSuit;
+
+  if (randomSuit === "♦" || randomSuit === "♥") {
+    document.getElementById("suit-top").style.color = "red";
+    document.getElementById("suit-bottom").style.color = "red";
+  } else {
+    document.getElementById("suit-top").style.color = "black";
+    document.getElementById("suit-bottom").style.color = "black";
+  }
+}
+window.onload = cardGenerator;
